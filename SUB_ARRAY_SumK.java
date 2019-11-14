7da4513a737165e139fac011e88d5e68ed94f03c
jagjotsk1999 <jagjotsk1999@gmail.com>
2019-11-14 12:52:57 +0530

    subarray sum indices

--- SUB_ARRAY_SumK.java
+++ SUB_ARRAY_SumK.java
@@ -4,30 +4,30 @@ import java.util.Scanner;
 
 public class SUB_ARRAY_SumK {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter length of array");
-        int n=sc.nextInt();
-        int[] arr=new int[n];
-        int sum=0,count=0;
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int sum = 0, count = 0;
         System.out.println("Enter the elements");
-        for (int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
         System.out.println("Enter the value of k");
-        int ksum=sc.nextInt();
-        for (int i=0;i<n;i++){
-            for (int j=0;j<n;j++){
-                for (int k=i;k<=j;k++){
-//                    System.out.print(arr[k]+" ");
-                    sum=sum+arr[k];
+        int ksum = sc.nextInt();
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            total = 0;
+            for (int j = i; j < n; j++) {
+                total = total + arr[j];
+                if (total == ksum) {
+                    int p = j;
+                    System.out.println(ksum + "->[" + i + "," + p + "]");
                 }
-                if (sum==ksum)
-                    count++;
-                sum=0;
-//                System.out.println();
+                if (total > ksum || n == j) {
+                    break;
+                }
             }
         }
-        System.out.println(count);
-//        System.out.println(sum);
     }
 }
